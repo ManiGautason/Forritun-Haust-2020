99918e9e348eec89bf92ea4df368a9a43890048a
ManiGautason <gautason.mani@gmail.com>
2020-02-03 11:05:08 +0000

    Add files via upload

---
+++ Æfingaverkefni 8.py
@@ -0,0 +1,84 @@
+import math
+#=========================================Verkefni 1====================================================
+
+# haetta = 0
+# while haetta == 0:
+#     tala = int(input("Sladu inn tolu: "))
+#     print("Tu hefur valid toluna ",tala)
+#     svar = input("viltu sla inn adra tolu? ")
+#
+#     if svar == "ja":
+#         print("Frabaert")
+#     else:
+#         print("Eigdu godan dag")
+#         haetta = 1
+
+#=========================================Verkefni 2====================================================
+
+# for i in range(5,16,1):
+#     print(i)
+
+#=========================================Verkefni 3====================================================
+
+# byrjun = int(input("Sladu in upphaf bils: "))
+# endir = int(input("Sladu in enda bils: "))
+#
+# for i in range(byrjun, endir + 1, 1):
+#     print(i)
+
+#=========================================Verkefni 4====================================================
+
+# byrjun = int(input("Sladu in upphaf bils: "))
+# endir = int(input("Sladu in enda bils: "))
+#
+# if byrjun == endir:
+#     print("villa reyndu aftur")
+# else:
+#     for i in range(byrjun, endir + 1, 1):
+#         print(i)
+
+#=========================================Verkefni 5====================================================
+
+# byrjun = int(input("Sladu in upphaf bils: "))
+# endir = int(input("Sladu in enda bils: "))
+#
+# for i in range(byrjun, endir + 1, 1):
+#     print(i)
+#
+# print("Herna eru svo tolurnar i odruveldi")
+#
+# for i in range(byrjun, endir + 1, 1):
+#     print(i**2)
+
+#=========================================Verkefni 6====================================================
+
+# byrjun = int(input("Sladu in upphaf bils: "))
+# endir = int(input("Sladu in enda bils: "))
+# x = 0
+# for i in range(byrjun, endir + 1, 1):
+#      x = x+i
+# print(x)
+
+#=========================================Verkefni 7====================================================
+
+# byrjun = int(input("Sladu in upphaf bils: "))
+# endir = int(input("Sladu in enda bils: "))
+#
+# hrop = 1
+# for i in range(byrjun, endir + 1, 1):
+#     hrop = hrop * i
+# print(hrop)
+
+#=========================================Verkefni 7====================================================
+
+# margfoldun = int(input("Sladu in margfoldunartoflu: "))
+#
+# for i in range(1, 11, 1):
+#     print(margfoldun*i, end=" ")
+
+#=========================================Verkefni 7====================================================
+
+# for i in range(1, 11, 1):
+#     print("")
+#     for x in range(1, 11, 1):
+#         print(x*i, end=" ")
